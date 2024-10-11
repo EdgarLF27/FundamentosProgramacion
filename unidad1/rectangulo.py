f44db05ec9e34b906eb0060a54a8a1ad4a91d7e0
Edgar Flores <e.flores30nov@gmail.com>
2024-10-11 13:19:53 -0600

    Rectangulo.py

---
+++ unidad1/rectangulo.py
@@ -0,0 +1,4 @@
+base = float(input("Ingresa la base del rectangulo "))
+altura =float(input ("Ingresa la altura del rectangulo "))
+area = base * altura
+print("El area del rectangulo es: " + str(area))
