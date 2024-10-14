44eeaa8e25ec9507431a3fa5ac327102d77483e6
Edgar Flores <e.flores30nov@gmail.com>
2024-10-14 10:22:58 -0600

    .

--- unidad1/Ejercicio3.py
+++ unidad1/Ejercicio3.py
@@ -2,5 +2,5 @@ import math
 a = int(input ("Escribe el valor del cateto A: "))
 b= int(input("Escribe el valor del cateto B: "))
 c = (a * a + b * b) ** (1/2)
-c = math.sqrt (a ** 2 + b ** 2)
+c = math.sqrt (a * 2 + b **b)
 print ("El valor de la hipotenusa es : ", c)
